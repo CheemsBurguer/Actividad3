5098c41dc2cc197d91c5be609f451490ba44b5f8
CheemsBurguer <strangercheems@gmail.com>
2025-08-30 21:38:58 -0600

    actividad terminada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.pougang.actividad3"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.pougang.actividad3"
--- app/src/main/java/com/pougang/actividad3/MainActivity.kt
+++ app/src/main/java/com/pougang/actividad3/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.pougang.actividad3
 
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var scaleFactor = 1.0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,40 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        Toast.makeText(this, "Hola pou", Toast.LENGTH_SHORT).show()
+
+        val tvHome = findViewById<TextView>(R.id.tvHome)
+
+        val btnHome = findViewById<Button>(R.id.btn_home)
+
+        btnHome.setOnClickListener {
+            if(tvHome.text == getString(R.string.poucificacion)) {
+                tvHome.text = getString(R.string.tv_home)
+            } else {
+                tvHome.text = getString(R.string.poucificacion)
+            }
+        }
+
+        val imgHome = findViewById<ImageView>(R.id.imageView)
+
+        val scaleGesture = ScaleGestureDetector(this,
+            object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+                override fun onScale(detector: ScaleGestureDetector): Boolean {
+                    scaleFactor *= detector.scaleFactor
+
+                    scaleFactor = scaleFactor.coerceIn(0.5f, 5.0f)
+
+                    imgHome.scaleX = scaleFactor
+                    imgHome.scaleY = scaleFactor
+                    return true
+                }
+            })
+
+        imgHome.setOnTouchListener { _, event ->
+            scaleGesture.onTouchEvent(event)
+            true
         }
     }
 }
